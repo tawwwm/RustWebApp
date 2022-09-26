8cea756d08b9e85ea3004c0f5d2b302b7a262c9e
Tom Rainford <tom_rainford@yahoo.co.uk>
2022-09-26 12:51:44 +0100

    Added Login/Logout Functionality

--- web-app/src/main.rs
+++ web-app/src/main.rs
@@ -5,6 +5,7 @@ pub mod models;
 
 // IMPORTS ###
 use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use actix_identity::{Identity, CookieIdentityPolicy, IdentityService};
 use tera::{Tera, Context};
 use serde::{Serialize, Deserialize};
 use diesel::prelude::*;
@@ -12,7 +13,7 @@ use diesel::pg::PgConnection;
 use dotenv::dotenv;
 
 // FILE IMPORTS #
-use models::{User, NewUser};
+use models::{User, NewUser, LoginUser};
 
 
 // STRUCTS ###
@@ -87,11 +88,11 @@ async fn register(tera: web::Data<Tera>) -> impl Responder{
 async fn register_user(data: web::Form<NewUser>) -> impl Responder{
     use schema::users;
 
-    let connection = establish_connection();
+    let mut connection = establish_connection();
 
     diesel::insert_into(users::table)
         .values(&*data)
-        .get_result::<User>(&connection)
+        .get_result::<User>(&mut connection)
         .expect("Error registering user.");
 
     println!("{:?}", data);
@@ -99,18 +100,47 @@ async fn register_user(data: web::Form<NewUser>) -> impl Responder{
 }
 
 //Login page
-async fn login(tera: web::Data<Tera>) -> impl Responder{
+async fn login(tera: web::Data<Tera>, id: Identity) -> impl Responder{
     let mut data = Context::new();
-
     data.insert("title", "Login");
+
+    if let Some(id) = id.identity(){
+        return HttpResponse::Ok().body("You are already logged in.")
+    }
+
     let rendered = tera.render("login.html", &data).unwrap();
     HttpResponse::Ok().body(rendered)
 }
 
 //Login form
-async fn login_user(data: web::Form<LoginAttempt>) -> impl Responder{
-    println!("{:?}", data);
-    HttpResponse::Ok().body(format!("Succesfully logged in as: {}", data.username))
+async fn login_user(data: web::Form<LoginUser>, id: Identity) -> impl Responder{
+    use schema::users::dsl::{username,users};
+
+    let mut connection = establish_connection();
+    let user = users.filter(username.eq(&data.username)).first::<User>(&mut connection);
+
+    match user{
+        Ok(u) => {
+            if u.password == data.password {
+                let session_token = String::from(u.username);
+                id.remember(session_token);
+                HttpResponse::Ok().body(format!("Succesfully logged in as: {}", data.username))
+            }else{
+                HttpResponse::Ok().body("Password is incorrect.")
+            }
+        },
+        Err(e) => {
+            println!("{:?}", e);
+            HttpResponse::Ok().body("User does not exist.")
+        }
+    }
+    
+    
+}
+
+async fn logout_user(id: Identity) -> impl Responder{
+    id.forget();
+    HttpResponse::Ok().body("Logged user out.")
 }
 
 //Threads page
@@ -134,12 +164,18 @@ async fn main() -> std::io::Result<()>{
     HttpServer::new(|| {
         let tera = Tera::new("templates/**/*").unwrap();
         App::new()
+            .wrap(IdentityService::new(
+                CookieIdentityPolicy::new(&[0;32])
+                .name("auth-cookie")
+                .secure(false)
+            ))
             .data(tera)
             .route("/", web::get().to(index))
             .route("/register", web::get().to(register))
             .route("/register", web::post().to(register_user))
             .route("/login", web::get().to(login))
             .route("/login", web::post().to(login_user))
+            .route("/logout", web::to(logout))
             .route("/post", web::get().to(thread))
             .route("/post", web::post().to(post_thread))
     })
--- web-app/src/models.rs
+++ web-app/src/models.rs
@@ -17,3 +17,9 @@ pub struct NewUser{
 	pub email: String,
 	pub password: String,
 }
+
+#[derive(Debug, Deserialize)]
+pub struct LoginUser{
+	pub username: String,
+	pub password: String,
+}
