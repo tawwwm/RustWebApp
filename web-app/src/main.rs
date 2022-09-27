2bf121df33cbd2b5043d4b971260760e0044cf8b
Tom Rainford <tom_rainford@yahoo.co.uk>
2022-09-27 15:59:46 +0100

    Improved Functionality
    - Improved Posting Thread Function
    - Added Commenting on Threads

--- web-app/src/main.rs
+++ web-app/src/main.rs
@@ -13,34 +13,31 @@ use diesel::pg::PgConnection;
 use dotenv::dotenv;
 
 // FILE IMPORTS #
-use models::{User, NewUser, LoginUser};
+use models::{User, NewUser, LoginUser, Thread, NewThread, Comment, NewComment};
 
 
 // STRUCTS ###
-#[derive(Serialize)]
-struct Thread {
-    title: String,
-    link: String,
-    author: String
-}
-
 #[derive(Deserialize, Debug)]
 struct PostAttempt {
     title: String,
     link: String
 }
 
-/*#[derive(Deserialize, Debug)]
-struct User{
-    username: String,
-    email: String,
-    password: String
-}*/
-
 #[derive(Deserialize, Debug)]
 struct LoginAttempt{
     username: String,
     password: String
+}
+
+#[derive(Deserialize, Debug)]
+struct PostForm{
+    title: String,
+    link: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct CommentForm{
+    content: String,
 }
 
 //FUNCTIONS###
@@ -57,19 +54,17 @@ fn establish_connection() -> PgConnection{
 
 
 async fn index(tera: web::Data<Tera>) -> impl Responder{
-    let mut data = Context::new();
-    
-    let posts = [
-        Thread{
-            title: String::from("Test Post"),
-            link: String::from("https://tera.netlify.app/docs/"),
-            author: String::from("Tawm")
-        }
-        
-    ];
+    use schema::threads::dsl::{threads};
+    use schema::users::dsl::{users};
 
+    let mut connection = establish_connection();
+    let thread_list :Vec<(Thread, User)> = threads.inner_join(users)
+        .load(&mut connection)
+        .expect("Error retrieving posts");
+    
+    let mut data = Context::new();
     data.insert("title", "Web App");
-    data.insert("posts", &posts);
+    data.insert("threadsInfo", &thread_list);
 
     let rendered = tera.render("index.html", &data).unwrap();
     HttpResponse::Ok().body(rendered)
@@ -144,19 +139,122 @@ async fn logout_user(id: Identity) -> impl Responder{
 }
 
 //Threads page
-async fn thread(tera: web::Data<Tera>) -> impl Responder{
+async fn thread(tera: web::Data<Tera>, id: Identity) -> impl Responder{
     let mut data = Context::new();
 
     data.insert("title", "Post Thread");
-    let rendered = tera.render("post.html", &data).unwrap();
-    HttpResponse::Ok().body(rendered)
+
+    if let Some(id) = id.identity(){
+        let rendered = tera.render("post.html", &data).unwrap();
+        return HttpResponse::Ok().body(rendered)
+    }
+
+    HttpResponse::Ok().body("User not logged in.")
 }
 
 //Thread submission form
-async fn post_thread(data: web::Form<PostAttempt>) -> impl Responder {
-    println!("{:?}", data);
-    HttpResponse::Ok().body(format!("Succesfully Posted Thread: {}", data.title))
+async fn post_thread(data: web::Form<PostForm>, id: Identity) -> impl Responder {
+
+    if let Some(id) = id.identity(){
+        use schema::users::dsl::{username, users};
+
+        let mut connection = establish_connection();
+        let user: Result<User, diesel::result::Error> = users.filter(username.eq(id)).first(&mut connection);
+
+        match user{
+            Ok(u) => {
+                let new_thread = NewThread::from_post_form(data.title.clone(), data.link.clone(), u.id);
+                use schema::threads;
+
+                diesel::insert_into(threads::table)
+                    .values(&new_thread)
+                    .get_result::<Thread>(&mut connection)
+                    .expect("Error posting thread.");
+
+                    return HttpResponse::Ok().body("Posted thread!");
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                return HttpResponse::Ok().body("Failed to find user.")
+            }
+        }
+
+    }
+    HttpResponse::Unauthorized().body("User not logged in.")
 }
+
+// Thread specific page
+async fn thread_page(tera: web::Data<Tera>, id: Identity, web::Path(thread_id): web::Path<i32>) -> impl Responder{
+
+    use schema::threads::dsl::{threads};
+    use schema::users::dsl::{users};
+
+    let mut connection = establish_connection();
+
+    let thread :Thread = threads.find(thread_id)
+        .get_result(&mut connection)
+        .expect("Failed to load thread.");
+    
+    let user :User = users.find(thread.authorid)
+        .get_result(&mut connection)
+        .expect("Failed to load thread author.");
+    
+    let mut data = Context::new();
+    data.insert("Title",&format!("{}", thread.title));
+    data.insert("thread", &thread);
+    data.insert("user", &user);
+
+    if let Some(_id) = id.identity() {
+        data.insert("logged_in", "true");
+    }else{
+        data.insert("logged_in", "false");
+    }
+
+    let rendered = tera.render("post.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)    
+}
+
+async fn comment(data: web::Form<CommentForm>, id:Identity, web::Path(thread_id): web::Path<i32>) -> impl Responder{
+
+    if let Some(id) = id.identity(){
+        use schema::threads::dsl::{threads};
+        use schema::users::dsl::{users};
+
+        let mut connection = establish_connection();
+
+        let thread :Thread = thread.find(thread_id)
+            .get_result(&mut connection)
+            .expect("Failed to find post.");
+        
+        let user :Result<User, diesel::result::Error> = users
+            .filter(username.eq(id))
+            .first(&mut connection);
+
+        match user{
+            Ok(u) => {
+                let parent_id = None;
+                let new_comment = NewComment::new(data.comment.clone(), post.id, u.id, parent_id);
+
+                use schema::comments;
+                diesel::insert_into(comments::table)
+                    .values(&new_comment)
+                    .get_result::<Comment>(&mut connection)
+                    .expect("Error processing comment.");
+
+                return HttpResponse::Ok().body("Comment posted.")
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                return HttpResponse::Ok().body("User not found.");
+            }
+
+        }
+    }
+
+    HttpResponse::Unauthorized().body("Not logged in.")
+
+}
+
 
 
 #[actix_web::main]
@@ -175,9 +273,14 @@ async fn main() -> std::io::Result<()>{
             .route("/register", web::post().to(register_user))
             .route("/login", web::get().to(login))
             .route("/login", web::post().to(login_user))
-            .route("/logout", web::to(logout))
+            .route("/logout", web::to(logout_user))
             .route("/post", web::get().to(thread))
             .route("/post", web::post().to(post_thread))
+            .service(
+                web::resource("/thread/{thread_id}")
+                    .route(web::get().to(thread_page))
+                    .route(web::post().to(comment))
+            )
     })
     .bind("127.0.0.1:8000")?
     .run()
--- web-app/src/models.rs
+++ web-app/src/models.rs
@@ -1,8 +1,8 @@
-use super::schema::users;
+use super::schema::{users, threads, comments};
 use diesel::{Queryable, Insertable};
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
-#[derive(Queryable)]
+#[derive(Serialize, Debug, Queryable)]
 pub struct User{
 	pub id: i32,
 	pub username: String,
@@ -22,4 +22,67 @@ pub struct NewUser{
 pub struct LoginUser{
 	pub username: String,
 	pub password: String,
+}
+
+#[derive(Serialize, Debug, Queryable)]
+pub struct Thread{
+	pub id: i32,
+	pub title: String,
+	pub link: Option<String>,
+	pub author_id: i32,
+	pub created_at: chrono::NaiveDateTime,
+}
+
+#[derive(Serialize, Insertable)]
+#[table_name="threads"]
+pub struct NewThread{
+	pub title: String,
+	pub link: String,
+	pub author_id: i32,
+	pub createdat: chrono::NaiveDateTime,
+}
+
+impl NewThread{
+	pub fn from_post_form(title: String, link: String, uid: i32) -> Self{
+		NewThread{
+			title: title,
+			link: link,
+			author_id: uid,
+			createdat: chrono::Local::now().naive_utc(),
+		}
+	}
+}
+
+
+#[derive(Serialize, Debug, Queryable)]
+pub struct Comment{
+	pub id: i32,
+	pub content: String,
+	pub thread_id: i32,
+	pub author_id: i32,
+	pub parent_comment_id: Option<i32>,
+	pub created_at: chrono::NaiveDateTime,
+}
+
+#[derive(Serialize, Insertable)]
+pub struct NewComment{
+	pub content: String,
+	pub thread_id: i32,
+	pub author_id: i32,
+	pub parent_comment_id: Option<i32>,
+	pub created_at: chrono::NaiveDateTime,
+}
+
+impl NewComment{
+	pub fn new(content: String, thread_id: i32, author_id: i32, parent_comment_id: Option<i32>) -> Self{
+		NewComment{
+			content: content,
+			thread_id: thread_id,
+			author_id: author_id,
+			parent_comment_id: parent_comment_id,
+			created_at: chrono::Local::now().naive_utc(),
+		}
+	}
+
+
 }
